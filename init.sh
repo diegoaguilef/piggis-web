76c49f63ee771ac864a6b96edf02801ff7445038
root <diego.aguilef@gmail.com>
2019-04-09 20:38:57 +0000

    updates init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 RAILS_ENV=production rails secret
+RAILS_ENV=production bundle exec rails assets:clobber
 RAILS_ENV=production bundle exec rails assets:precompile
+RAILS_ENV=production bundle exec rails assets:clean
 RAILS_ENV=production bundle exec rails db:migrate
 bundle exec rails s -p 3000 -b '0.0.0.0' -e production
